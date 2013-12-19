b38e805f6a88e9e4d7f4821b4b108dd44a17a63b
matisdim <matiasdim3@gmail.com>
2013-12-18 19:42:46 -0500

    Flip show random card. 12 cards

--- matchismo/matchismo/CardGameViewController.m
+++ matchismo/matchismo/CardGameViewController.m
@@ -7,14 +7,28 @@
 //
 
 #import "CardGameViewController.h"
+#import "Deck.h"
+#import "PlayingCardDeck.h"
 
 @interface CardGameViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *flipsLabel;
 @property (nonatomic) int flipCount;
+@property (strong, nonatomic) Deck *deck;
 
 @end
 
 @implementation CardGameViewController
+
+- (Deck *)deck
+{
+    if (!_deck) _deck = [self createDeck];
+    return _deck;
+}
+
+- (Deck *)createDeck
+{
+    return [[PlayingCardDeck alloc] init];
+}
 
 - (void) setFlipCount:(int)flipCount{
     _flipCount = flipCount;
@@ -27,9 +41,13 @@
                           forState:UIControlStateNormal];
         [sender setTitle:@"" forState:UIControlStateNormal];
     }else{
-        [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
-                          forState:UIControlStateNormal];
-        [sender setTitle:@"A♣" forState:UIControlStateNormal];
+        Card *randomCard = [self.deck drawRandomCard];
+        if (randomCard) {
+            [sender setBackgroundImage:[UIImage imageNamed:@"cardfront"]
+                              forState:UIControlStateNormal];
+            [sender setTitle:randomCard.contents forState:UIControlStateNormal];
+        }
+        
     }
     self.flipCount++;
 }
--- matchismo/matchismo/Model/PlayingCard.m
+++ matchismo/matchismo/Model/PlayingCard.m
@@ -12,7 +12,7 @@
 
 - (NSString *)contents
 {
-    NSString *rankStrings = [PlayingCard rankStrings];
+    NSArray *rankStrings = [PlayingCard rankStrings];
     return [rankStrings[self.rank] stringByAppendingString:self.suit];
 }
 
@@ -35,10 +35,14 @@
     return _suit ? _suit : @"?";
 }
 
-+ (NSString *)rankStrings
++ (NSArray *)rankStrings
 {
-    return @[@"?",@"A",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",
-             @"J",@"Q",@"K"];
+    return @[@"?",@"A",@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"J",@"Q",@"K"];
+}
+
++ (NSUInteger)maxRank
+{
+    return [self rankStrings].count - 1;
 }
 
 @end
